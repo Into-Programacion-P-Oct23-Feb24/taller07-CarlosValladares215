b0d35694e392270d27c5ca3346509fe18752426c
Carlos Valladares <csvalladares2@utpl.edu.ec>
2023-11-26 22:07:30 -0500

    Problema 04

---
+++ problematicas/Problema_04/src/problema_04/Problema_04.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package problema_04;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author vcarl
+ */
+public class Problema_04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+
+        // Variables
+        String acumulador = "1";
+        String signo;
+        int aumento = 1;
+        int contador = 1;
+        
+        // while
+        while (contador < 8) {
+            aumento = aumento + 2;
+            if (contador % 2 == 0) {
+                signo = "+";
+            }else{
+                signo = "-";
+            }
+            acumulador = String.format("%s %s 1/%d"
+                    , acumulador
+                    ,signo
+                    ,aumento);
+            contador = contador +1;
+        }
+        
+        // Se presenta por pantalla el resultado
+        System.out.printf("%s \n", acumulador);
+    }
+
+}
